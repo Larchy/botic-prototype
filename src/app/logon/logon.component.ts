fb799b02099a7016dcc810c37fda95d441b5c902
larchy <larchy@gmail.com>
2017-01-22 16:19:06 -0600

    Signup with email stuff

--- src/app/logon/logon.component.ts
+++ src/app/logon/logon.component.ts
@@ -1,10 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, transition, style, animate } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-logon',
   templateUrl: './logon.component.html',
-  styleUrls: ['./logon.component.scss']
+  styleUrls: ['./logon.component.scss'],
+  animations: [
+    trigger(
+      'myAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(-100%)', opacity: 0}),
+          animate('250ms', style({transform: 'translateX(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)', 'opacity': 1}),
+          animate('250ms', style({transform: 'translateX(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ],
 })
 export class LogonComponent implements OnInit {
   user: {
@@ -21,7 +35,11 @@ export class LogonComponent implements OnInit {
   }
 
   login () {
-	  this.router.navigateByUrl(`/profile`)
+	  this.router.navigateByUrl('/profile')
+  }
+
+  signup () {
+    this.router.navigateByUrl('/security')
   }
 
 }
