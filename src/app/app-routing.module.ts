2dbad51277bb277970afc3da29840335d49d49c8
Tim Burnell <tmburnell@gmail.com>
2017-01-24 22:42:51 -0600

    start event view and summary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { LogonComponent }         from './logon/logon.component';
 import { SecuritySetupComponent } from './security-setup/security-setup.component';
 import { ProfileComponent }       from './profile/profile.component';
 import { ProfileSetupComponent }  from './profile-setup/profile-setup.component';
+import { ProfileViewComponent }   from './profile-view/profile-view.component';
 import { LiveComponent }          from './live/live.component';
 import { EventSetupComponent }    from './event-setup/event-setup.component';
+import { EventSummaryComponent }    from './event-summary/event-summary.component';
 import { NotFoundComponent }      from './not-found/not-found.component';
 import {BankInfoComponent}        from "./bank-info/bank-info.component";
 import {SyncComponent}            from "./sync/sync.component";
@@ -16,9 +18,11 @@ const appRoutes: Routes = [
   { path: 'bankInfo', component: BankInfoComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'profile-setup', component: ProfileSetupComponent },
+  { path: 'profile-view', component: ProfileViewComponent },
   { path: 'sync', component: SyncComponent },
   { path: 'live', component: LiveComponent },
   { path: 'eventSetup', component: EventSetupComponent },
+  { path: 'event-summary', component: EventSummaryComponent },
   { path: '',   redirectTo: '/logon', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile-view',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
+	private feeds: Array<any>;
+	private name:string;
+	private video: any = {id: 'tJJdBTQqwQM'};
+	private baseUrl:string = 'https://www.youtube.com/embed/';
+	private url:any;
 
-  constructor() { }
+  constructor(private sanitizer:DomSanitizer) { }
 
   ngOnInit() {
+	  this.feeds = [
+          {name: "@RichJay", id: 'tJJdBTQqwQM', tumbnail: 'cook-01.png'},
+          {name: "@MelvinJamie", id: 'dvlEaUlTEsg', tumbnail: 'cook-02.png'},
+          {name: "@AngelaJones", id: 'Mx1DxPwEz1w', tumbnail: 'cook-03.png'},
+          {name: "@DougSmith", id: 'WtH3dBbEDIE', tumbnail: 'cook-04.png'}
+	  ];
+
+	  this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id + "?autoplay=1" + "rel=0&showinfo=0&autoplay=1");
   }
 
+  invite() {
+	  debugger;
+	  // TODO: do something;
+  }
+
+  selectVideo(videoId) {
+	  this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + videoId + "?autoplay=1" + "rel=0&showinfo=0&autoplay=1");
+  }
 }
